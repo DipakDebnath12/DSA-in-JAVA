29389cd876abfdd3453d97385c20b54c8306e930
Dipak Debnath <debnathdipak449@gmail.com>
2026-09-02 22:42:13 +0530

    Time: 1 ms (98.11%) | Memory: 42.1 MB (97.66%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,6 +1,5 @@
 class Solution {
     public int addDigits(int num) {
-        int res = (num == 0) ? 0 : ((num-1)%9)+1;
-        return res;
+        return (num == 0) ? 0 : ((num-1)%9)+1;
     }
 }
